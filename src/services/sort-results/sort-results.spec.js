0913591938ddc886b6797fd218aa6de767a32a55
Travis Huss <travisjhuss@gmail.com>
2021-04-30 12:30:33 -0500

    merging with new branch, feature-SortResultsService

--- src/services/sort-results/sort-results.spec.js
+++ src/services/sort-results/sort-results.spec.js
@@ -154,31 +154,117 @@ describe('SortResultsService', () => {
     ]);
   });
 
-//   it('will do this if given no price when ascending', () => {
-//     expect(
-//       SortResultsService(
-//         [
-//           ...testArrayOfHotels,
-//           {
-//             id: 5,
-//             hotelStaticContent: {
-//               name: 'FF Hotel Name',
-//               neighborhoodName: 'Logan Square',
-//               mainImage: {
-//                 url: 'https://www.website.com/photo.png',
-//               },
-//             },
-//             lowestAveragePrice: {
-//               symbol: '&#36;',
-//               amount: undefined,
-//             },
-//             rewards: {
-//               miles: 5000,
-//             },
-//           },
-//         ],
-//         'ascending'
-//       )
-//     ).toEqual();
-//   });
+  it('will do this if given no price when ascending', () => {
+    expect(
+      SortResultsService(
+        [
+          ...testArrayOfHotels,
+          {
+            id: 5,
+            hotelStaticContent: {
+              name: 'FF Hotel Name',
+              neighborhoodName: 'Logan Square',
+              mainImage: {
+                url: 'https://www.website.com/photo.png',
+              },
+            },
+            lowestAveragePrice: {
+              symbol: '&#36;',
+              amount: undefined,
+            },
+            rewards: {
+              miles: 5000,
+            },
+          },
+        ],
+        'ascending'
+      )
+    ).toEqual([
+      {
+        id: 5,
+        hotelStaticContent: {
+          name: 'FF Hotel Name',
+          neighborhoodName: 'Logan Square',
+          mainImage: {
+            url: 'https://www.website.com/photo.png',
+          },
+        },
+        lowestAveragePrice: {
+          symbol: '&#36;',
+          amount: undefined,
+        },
+        rewards: {
+          miles: 5000,
+        },
+      },
+      {
+        id: 1,
+        hotelStaticContent: {
+          name: 'AA Hotel Name',
+          neighborhoodName: 'Logan Square',
+          mainImage: {
+            url: 'https://www.website.com/photo.png',
+          },
+        },
+        lowestAveragePrice: {
+          symbol: '&#36;',
+          amount: 100,
+        },
+        rewards: {
+          miles: 5000,
+        },
+      },
+      {
+        id: 2,
+        hotelStaticContent: {
+          name: 'AB Hotel Name',
+          neighborhoodName: 'Ukrainian Village',
+          mainImage: {
+            url: 'https://www.website.com/photo.png',
+          },
+        },
+        lowestAveragePrice: {
+          symbol: '&#36;',
+          amount: 200,
+        },
+        rewards: {
+          miles: 3000,
+        },
+      },
+      {
+        id: 4,
+        hotelStaticContent: {
+          name: 'BC Hotel Name',
+          neighborhoodName: 'Magnificent Mile',
+          mainImage: {
+            url: 'https://www.website.com/photo.png',
+          },
+        },
+        lowestAveragePrice: {
+          symbol: '&#36;',
+          amount: 200,
+        },
+        rewards: {
+          miles: 5000,
+        },
+      },
+      {
+        id: 3,
+        hotelStaticContent: {
+          name: 'CC Hotel Name',
+          neighborhoodName: 'Magnificent Mile',
+          mainImage: {
+            url: 'https://www.website.com/photo.png',
+          },
+        },
+        lowestAveragePrice: {
+          symbol: '&#36;',
+          amount: 300,
+        },
+        rewards: {
+          miles: 4000,
+        },
+      },
+    ]);
+  });
 });
